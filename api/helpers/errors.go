e01f21f8664f944bc750d4d4db8de173325a25e2
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-09-04 15:08:02 +0100

    update error message

--- api/helpers/errors.go
+++ api/helpers/errors.go
@@ -2,6 +2,6 @@ package helpers
 
 import "errors"
 
-var ErrConflict = errors.New("user with details already exists")
+var ErrConflict = errors.New("record already exists")
 var ErrNotFound = errors.New("resource not found")
 var ErrInternalServer = errors.New("internal server error")
